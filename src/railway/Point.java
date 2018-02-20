7800b2d4e031f5f984ea6247556f2de1c3a0f92e
leorickayzen <l.rickayzen1@newcastle.ac.uk>
2018-02-20 16:26:32 +0000

    added functionality for a point

--- src/railway/Point.java
+++ src/railway/Point.java
@@ -1,4 +1,37 @@
 package railway;
 
-public class Point {
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
+
+public class Point extends Group {
+    public Point(double start, double end, Boolean reverse){
+
+        double upperY;
+        double lowerY;
+
+        if(reverse){
+            upperY = 200.0;
+            lowerY = 150.0;
+        }else{
+            upperY = 150.0;
+            lowerY = 200.0;
+        }
+
+        Line upper = new Line();
+        upper.setStroke(Color.BLUE);
+        Line lower = new Line();
+        lower.setStroke(Color.BLUE);
+
+        upper.setStartX(start);
+        lower.setStartX(start);
+        upper.setEndX(end);
+        lower.setEndX(end);
+
+        upper.setStartY(lowerY);
+        upper.setEndY(upperY);
+        lower.setStartY(200.0);
+        lower.setEndY(200.0);
+        this.getChildren().addAll(upper, lower);
+    }
 }
--- src/railway/main.java
+++ src/railway/main.java
@@ -12,16 +12,22 @@ public class main extends Application{
      */
     public void start(Stage stage) {
 
-        TrackSection a1 = new TrackSection(150.0, 450.0, false);
-        TrackSection a2 = new TrackSection(460.0, 760.0, true);
-        TrackSection a3 = new TrackSection(770.0, 1070.0, false);
+        TrackSection a1 = new TrackSection(150.0, 450.0, true);
+        Point p1 = new Point(450, 460, false);
+        TrackSection a2 = new TrackSection(460.0, 760.0, false);
+        TrackSection a4 = new TrackSection(460.0, 760.0, true);
+        Point p2 = new Point(760, 770, true);
+        TrackSection a3 = new TrackSection(770.0, 1070.0, true);
 
         //Creating a Group
         Group root = new Group();
 
         root.getChildren().add(a1);
+        root.getChildren().add(p1);
         root.getChildren().add(a2);
+        root.getChildren().add(p2);
         root.getChildren().add(a3);
+        root.getChildren().add(a4);
 
         //Creating a Scene
         Scene scene = new Scene(root, 600, 300);
