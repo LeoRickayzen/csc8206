1bedb6100675e583982466ed21e7919341b24a84
Ohudm <36713708+Ohudm@users.noreply.github.com>
2018-02-24 12:42:01 +0000

    added functions

--- src/railway/Journey.java
+++ src/railway/Journey.java
@@ -1,6 +1,54 @@
 import java.util.ArrayList;
 
 public class Journey {
-	ArrayList<Route> Routes;
+	ArrayList<Route> routes;
+	
+	//Constructor
+	public Journey() {
+		this.routes=new ArrayList<Route>();
+		
+		}
+		
+    //Constructor
+	public Journey(ArrayList<Route> routes) {
+		this.routes=routes;
+		
+		}
+	/**
+     * set  routes list
+     *
+     * @param  routes  new routes list
+     */
+	public void setRoutes(ArrayList<Route> routes) {
+		this.routes=routes;
+	}
+	/**
+     * get routes list
+     *
+     * @return    a list of routes
+     */
+	public ArrayList<Route> getRoutes(){
+		return this.routes;
+	}
+	/**
+     * add route to a journey 
+     *
+     * @param  route  new route
+     * @return true if it is added successfully
+     */
+	public boolean addRoutesToJourney(Route route) {
+		return this.routes.add(route);
+	}
+	
+	/**
+     * remove route from journey 
+     *
+     * @param  route  existing route
+     * @return true if it is removed successfully
+     */
+	public boolean removeRouteFromJourney(Route route) {
+		return this.routes.remove(route);
+	}
+	
 
 }
