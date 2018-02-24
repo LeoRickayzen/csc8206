93a265d52231b668a944cb61ca2b965e67b49a2a
Ohudm <36713708+Ohudm@users.noreply.github.com>
2018-02-24 12:24:46 +0000

    Update Point.java
    added functions

--- src/railway/Point.java
+++ src/railway/Point.java
@@ -13,6 +13,41 @@ public class Point {
 		this.pNeigh=pNeigh;
 		
 	}
+	
+	public void setPlus(boolean plus) {
+		this.plus=plus;
+	}
+	
+	public boolean getPlus() {
+		return plus;
+	}
 
+	public void setMainNeigh(Section mainNeigh) {
+		this.mainNeigh=mainNeigh;
+		
+	}
 
+	public Section getmainNeigh() {
+		return mainNeigh;
+	}
+	
+	public void setMNeigh(Section mNeigh) {
+		this.mNeigh=mNeigh;
+	}
+	
+	public Section getPMeigh() {
+		return mNeigh;
+		
+	}
+	
+	public void setPNeigh(Section pNeigh) {
+		this.pNeigh=pNeigh;
+		
+	}
+	
+	public Section getPNeigh() {
+		return pNeigh;
+	}
+	
+	
 }
