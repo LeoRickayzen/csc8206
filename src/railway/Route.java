4d7b05419a75cfc3e99d60f1b287d9efdc3bd74f
Ohudm <36713708+Ohudm@users.noreply.github.com>
2018-02-24 12:37:25 +0000

    Update Route.java
    added functions

--- src/railway/Route.java
+++ src/railway/Route.java
@@ -2,5 +2,51 @@
 public class Route {
 	Signal source;
 	Signal destination;
+	
+	//Constructor
+	public Route(Signal source, Signal destination) {
+		this.source=source;
+		this.destination=destination;	
+	}
+	
+	/**
+     * set route's source
+     *
+     * @param  source  new route's source
+     */
+
+	public void setSource(Signal source) {
+		this.source=source;
+	}
+	
+    /**
+     * get route's source
+     *
+     * @return    route's source
+     */
+	public Signal getSource() {
+		return this.source;
+	}
+	
+	/**
+     * set destination
+     *
+     * @param  destination  new route's destination
+     */
+	public void setDestination(Signal destination) {
+		this.destination=destination;
+	}
+	/**
+     * get destination
+     *
+     * @return    route's destination
+     */
+	public Signal getDestination() {
+		return this.destination;
+	}
+	
+	
+	
+	
 
 }
