bcdeb5810e378bf6e7eb0007c6618db1df36fd18
Ohudm <36713708+Ohudm@users.noreply.github.com>
2018-02-24 12:34:42 +0000

    Update Signal.java
    added functions

--- src/railway/Signal.java
+++ src/railway/Signal.java
@@ -14,6 +14,72 @@ public class Signal {
 		this.upNeigh=upNeigh;
 		this.downNeigh=downNeigh;
 	}
+	/**
+     * set signal's block
+     *
+     * @param  blockID  new signal's block
+     */
+	//the section that the signal on
+	public void setBlockID(Section blockID) {
+		this.blockID=blockID;
+		
+	}
+	//the section that the signal on
+	public Section getBlockID() {
+		return blockID;
+	}
+	
+	/**
+     * set signal's status
+     *
+     * @param  clear  new clear value 
+     */
+	public void setClear(boolean clear) {
+		this.clear=clear;
+	}
+	
+	/**
+     * get signal's status 
+     *
+     * @return    true if it is clear
+     */
+	public boolean getClear() {
+		return clear;
+	}
+	
+	/**
+     * set signal's direction
+     *
+     * @param  direction  new direction value (up or down)
+     */
+	public void setDirection(String direction) {
+		this.direction=direction;
+	}
+	
+	/**
+     * get signal's direction 
+     *
+     * @return    up or down
+     */
+	public String getDirection() {
+		return direction;
+	}
+	
+	public void setUpNeigh(Section upNeigh) {
+		this.upNeigh=upNeigh;
+	}
+	
+	public Section getUpNeigh() {
+		return upNeigh;
+	}
+	
+	public void setDownNeigh(Section downNeigh) {
+		this.downNeigh=downNeigh;
+	}
+	
+	public Section getDownNeigh() {
+		return this.downNeigh;
+	}
 
 
 }
