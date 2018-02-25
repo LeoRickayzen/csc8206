adc4349dfeb0ffa4eb1b465231fdd9b0efd87cfd
Ohudm <36713708+Ohudm@users.noreply.github.com>
2018-02-25 19:51:31 +0000

    Add files via upload

---
+++ src/railway/Block.java
@@ -0,0 +1,19 @@
+
+public class Block {
+	int blockID;
+	
+	//constructor
+	public Block(int blockID) {
+		this.blockID=blockID;
+	}
+	
+	public void setBlockID(int blockID) {
+		  this.blockID=blockID;
+		  
+	  }
+	  
+	public int getBlockID() {
+		  return blockID;
+	  }
+
+}
