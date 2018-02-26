ef35582291034b9ece122832cac05094c842777f
Ohudm <36713708+Ohudm@users.noreply.github.com>
2018-02-26 12:43:39 +0000

    Update Route.java

--- src/railway/Route.java
+++ src/railway/Route.java
@@ -1,13 +1,17 @@
+import java.util.ArrayList;
 
 public class Route {
 	int routeID;
 	Signal source;
 	Signal destination;
+	ArrayList<Block> blocks;
 	
 	//Constructor
-	public Route(Signal source, Signal destination) {
+	public Route(int routID, Signal source, Signal destination,ArrayList<Block> blocks) {
+		this.routeID=routeID;
 		this.source=source;
-		this.destination=destination;	
+		this.destination=destination;
+		this.blocks=blocks;
 	}
 	
 	public void setRouteID(int routeID) {
@@ -55,8 +59,41 @@ public class Route {
 		return this.destination;
 	}
 	
+	/**
+     * set a block list
+     *
+     * @param  blocks  new block list
+     */
+	public void setBlocks(ArrayList<Block> blocks) {
+		this.blocks=blocks;
+	}
 	
+    /**
+     * get a block list
+     *
+     * @return    a list of block
+     */
+	public ArrayList<Block> getBlocks(){
+		return this.blocks;
+	}
 	
-	
+	/**
+     * add block to a route 
+     *
+     * @param  block  new block
+     * @return true if it is added successfully
+     */
+	public boolean addBlockToRoute(Block block) {
+		return this.blocks.add(block);
+	}
+	/**
+     * remove block from a route 
+     *
+     * @param  block  existing block
+     * @return true if it is removed successfully
+     */
+	public boolean removeBlockFromRoute(Block block) {
+		return this.blocks.remove(block);
+	}	
 
 }
