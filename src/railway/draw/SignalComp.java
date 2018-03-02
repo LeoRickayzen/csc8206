99b196c56e20d7c25486951a7bc0826fe1efc207
leorickayzen <l.rickayzen1@newcastle.ac.uk>
2018-03-02 14:30:11 +0000

    refactored and commented drawing code functionality, added functionality for drawing signals in reverse

--- src/railway/draw/SignalComp.java
+++ src/railway/draw/SignalComp.java
@@ -6,35 +6,43 @@ import javafx.scene.shape.Line;
 
 public class SignalComp extends Component {
 
+	int reverseConstant = 1;
 
     public SignalComp(double[] start, Boolean reverse, int id){
         
     	super(id);
     	
+    	if(reverse){
+    		reverseConstant = -1;
+    	}
+    	
         Line leftUp = new Line();
         leftUp.setStartX(start[0]);
         leftUp.setEndX(start[0]);
         leftUp.setStartY(start[1]);
-        leftUp.setEndY(start[1]-5);
+        leftUp.setEndY(start[1]-(reverseConstant*15));
         leftUp.setStroke(Color.ALICEBLUE);
         leftUp.setStrokeWidth(2);
-
+        
+        /*
         Line rightUp = new Line();
-        rightUp.setStartX(start[0]+5);
-        rightUp.setEndX(start[0]+5);
+        rightUp.setStartX(start[0]+2);
+        rightUp.setEndX(start[0]+2);
         rightUp.setStartY(start[1]);
-        rightUp.setEndY(start[1]-5);
+        rightUp.setEndY(start[1]-15);
         rightUp.setStroke(Color.ALICEBLUE);
         rightUp.setStrokeWidth(2);
 
         Line top = new Line();
-        top.setStartY(start[1]-5);
-        top.setEndY(start[1]-5);
+        top.setStartY(start[1]-15);
+        top.setEndY(start[1]-15);
         top.setStartX(start[0]);
-        top.setEndX(start[0]+5);
+        top.setEndX(start[0]+2);
         top.setStroke(Color.ALICEBLUE);
         top.setStrokeWidth(2);
 
         this.getChildren().addAll(leftUp, top, rightUp);
+        */
+        this.getChildren().addAll(leftUp);
     }
 }
