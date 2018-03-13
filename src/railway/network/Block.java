60ce12fb8c58584831f018985c9c1f83ac53d315
Ohudm <36713708+Ohudm@users.noreply.github.com>
2018-03-13 12:50:14 +0000

    added comments

--- src/railway/network/Block.java
+++ src/railway/network/Block.java
@@ -2,6 +2,10 @@ package railway.network;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+/**
+ * <p>Stores information about a Block.</p>
+ * 
+ */
 
 public class Block {
 	private int id;
@@ -12,15 +16,27 @@ public class Block {
 	Block(){}
 
 	//constructor
+	/**
+	 * 
+	 * @param id int ID of the Block
+	 */
 	Block(int id) {
 		this.id=id;
 	}
 	
+	/**
+	 * to set block ID
+	 * @param id int ID of the Block
+	 */
 	public void setId(int id) {
 		  this.id=id;
 		  
 	  }
-	  
+	 
+	/**
+	 * to get block ID
+	 * @return id int ID of the Block
+	 */
 	public int getId() {
 		  return id;
 	  }
@@ -43,32 +59,60 @@ public class Block {
 		return this.currentDirection.name().toLowerCase();
 	}*/
 
+	/**
+	 * to check if it has next block ID
+	 *
+	 * @param reverse Boolean 
+	 * @return ture
+	 */
 	@JsonIgnore
 	public Boolean hasNext(Boolean reverse){
 		//What an unhelpful function
 		return true;
 	}
-
+	
+	/**
+	 * to check if it has next block ID
+	 *
+	 * @param reverse Boolean 
+	 * @return 0
+	 */
 	@JsonIgnore
 	public int getNext(Boolean reverse){
 		return 0;
 	}
-
+	/**
+	 * to get block level
+	 * 
+	 * @return the block level
+	 */
 	@JsonIgnore
 	public int getLevel() {
 		return level;
 	}
-
+	/**
+	 * to set block level
+	 * 
+	 * @param the block level
+	 */
 	@JsonIgnore
 	public void setLevel(int level) {
 		this.level = level;
 	}
-
+	/**
+	 * to set block index
+	 * 
+	 * @param the block index
+	 */
 	@JsonIgnore
 	public void setIndex(int index){
 		this.index = index;
 	}
-
+	/**
+	 * to get block index
+	 * 
+	 * @return the block index
+	 */
 	@JsonIgnore
 	public int getIndex(){
 		return index;
